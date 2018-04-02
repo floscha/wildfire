56083b1af4c32b33159d4fe8abd3761340f25e30
Florian Schäfer <florian.joh.schaefer@gmail.com>
2018-04-02 09:38:14 +0200

    Add setup.py (#4)

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+
+VERSION = '0.0'
+
+requirements = list(open('requirements.txt', 'r').readlines())
+
+setup(
+    name='wildfire',
+    version=VERSION,
+    description='A library for automatically generating HTTP services',
+    url='https://github.com/floscha/floscha',
+    author='Florian Schäfer',
+    author_email='florian.joh.schaefer@gmail.com',
+    license='Apache Software License',
+    packages=['wildfire'],
+    install_requires=requirements,
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: Software Development'
+    ]
+)
